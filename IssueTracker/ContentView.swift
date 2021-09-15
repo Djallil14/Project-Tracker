60e63c90d19e9c903405ae787f66f1aefcd3e62c
Djallil Elkebir <elkebir.djallil@icloud.com>
2021-09-14 22:23:04 -0400

    Initial Commit

---
+++ IssueTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IssueTracker
+//
+//  Created by Djallil Elkebir on 2021-09-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IssueTracker/IssueTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IssueTrackerApp.swift
+//  IssueTracker
+//
+//  Created by Djallil Elkebir on 2021-09-14.
+//
+
+import SwiftUI
+
+@main
+struct IssueTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
